cdf931c8e36babf5f63ec68374bbbc4939f790b4
nicholasmdoherty <nicholasmdoherty@gmail.com>
2020-08-27 13:17:55 -0400

    [deploy] initial test deploy of brian's website

---
+++ precache-manifest.76d7ecd4784efd87eeb75bba4214fb73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bec31955f0e19f98df3753ce108de51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3758046dd6c153f3229",
+    "url": "/static/css/main.b58a9622.chunk.css"
+  },
+  {
+    "revision": "8aa46d133420c3ab2f25",
+    "url": "/static/js/2.62cd95af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.62cd95af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3758046dd6c153f3229",
+    "url": "/static/js/main.29158b96.chunk.js"
+  },
+  {
+    "revision": "15e0248a70979145621c",
+    "url": "/static/js/runtime-main.29cddc05.js"
+  }
+]);
